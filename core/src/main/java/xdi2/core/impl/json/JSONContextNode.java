a2e1b13f690bf24aeac7d7d1738c7614e0354e70
kvats <kapil.vats@impetus.co.in>
2016-06-08 16:40:53 +0530

    1. Fixed ConcurrentModificationException while creating HashSet object by passing JsonObject.entrySet() as constructor argument.
    2. Fixed NullPointerException by adding Null check.

--- core/src/main/java/xdi2/core/impl/json/JSONContextNode.java
+++ core/src/main/java/xdi2/core/impl/json/JSONContextNode.java
@@ -232,7 +232,10 @@ public class JSONContextNode extends AbstractContextNode implements ContextNode 
 
 		JsonObject jsonObject = ((JSONGraph) this.getGraph()).jsonLoad(this.getXDIAddress().toString());
 
-		final Set<Entry<String, JsonElement>> entrySet = new HashSet<Entry<String, JsonElement>> (jsonObject.entrySet());
+		final Set<Entry<String, JsonElement>> entrySet;
+		synchronized (this) {
+		    entrySet = new CopyOnWriteArraySet<Entry<String, JsonElement>>(jsonObject.entrySet());
+                }
 
 		return new DescendingIterator<Entry<String, JsonElement>, Relation> (entrySet.iterator()) {
 
